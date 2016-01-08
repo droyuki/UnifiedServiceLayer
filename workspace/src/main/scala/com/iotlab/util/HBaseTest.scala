b82e6834aec134deb6b201975954fb0c403f183e
Wilson Liao <droyuki@gmail.com>
2016-01-09 02:38:30 +0800

    add HBaseTest.scala

--- workspace/src/main/scala/com/iotlab/util/HBaseTest.scala
+++ workspace/src/main/scala/com/iotlab/util/HBaseTest.scala
@@ -7,7 +7,6 @@ package com.iotlab.util
 import org.apache.hadoop.hbase.util.Bytes
 import org.apache.hadoop.hbase.{HColumnDescriptor, HTableDescriptor, TableName, HBaseConfiguration}
 import org.apache.hadoop.hbase.client._
-import org.apache.spark.SparkContext
 import scala.collection.JavaConversions._
 
 object HBaseTest {
@@ -29,7 +28,7 @@ object HBaseTest {
 
     val userTable = TableName.valueOf("TestTable")
 
-    //创建 user 表
+    //create table
     val tableDescr = new HTableDescriptor(userTable)
     tableDescr.addFamily(new HColumnDescriptor("basic".getBytes))
     println("Creating table `TestTable`. ")
@@ -41,24 +40,23 @@ object HBaseTest {
     println("Done!")
 
     try {
-      //获取 user 表
+      //get table
       val table = conn.getTable(userTable)
 
       try {
-        //准备插入一条 key 为 id001 的数据
+        //插入key = id001 的資料
         val p = new Put("id001".getBytes)
-        //为put操作指定 column 和 value （以前的 put.add 方法被弃用了）
+        //為put操作指定 column 和 value （舊的put.add 方法被deprecated了）
         p.addColumn("basic".getBytes, "name".getBytes, "wuchong".getBytes)
-        //提交
         table.put(p)
 
-        //查询某条数据
+        //查詢
         val g = new Get("id001".getBytes)
         val result = table.get(g)
         val value = Bytes.toString(result.getValue("basic".getBytes, "name".getBytes))
         println("GET id001 :" + value)
 
-        //扫描数据
+        //掃描
         val s = new Scan()
         s.addColumn("basic".getBytes, "name".getBytes)
         val scanner = table.getScanner(s)
@@ -69,11 +67,10 @@ object HBaseTest {
             println("Found value: " + Bytes.toString(r.getValue("basic".getBytes, "name".getBytes)))
           }
         } finally {
-          //确保scanner关闭
           scanner.close()
         }
 
-        //删除某条数据,操作方式与 Put 类似
+        //Delete data
         val d = new Delete("id001".getBytes)
         d.addColumn("basic".getBytes, "name".getBytes)
         table.delete(d)
